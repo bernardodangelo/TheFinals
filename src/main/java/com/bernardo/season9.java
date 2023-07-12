adb7a5e109b91cf6a8b9782ff164e30830f3cb33
bernardo <bernardodangelo8@gmail.com>
2023-07-12 01:08:00 -0300

    feat: add chance to pig spawn with the name technoblade

--- src/main/java/com/bernardo/season9.java
+++ src/main/java/com/bernardo/season9.java
@@ -1,17 +1,45 @@
 package com.bernardo;
 
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class season9 extends JavaPlugin {
+import java.util.Random;
+
+public final class season9 extends JavaPlugin implements Listener {
+    private Random random = new Random();
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getLogger().info("Season 9 Plugin Iniciado.");
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("Season 9 Plugin Finalizado.");
+    }
+
+    @EventHandler
+    public void onCreatureSpawn(CreatureSpawnEvent event) {
+        if (event.getEntityType() == EntityType.PIG) {
+            if (random.nextDouble() <= 0.005) {
+                event.getEntity().setCustomName("Technoblade");
+                event.getEntity().setCustomNameVisible(true);
+            }
+        }
+    }
+    @EventHandler
+    public void onCreatureSpawnNatural(CreatureSpawnEvent event) {
+        if (event.getEntityType() == EntityType.PIG && event.getSpawnReason() == SpawnReason.NATURAL) {
+            if (random.nextDouble() <= 0.005) {
+                event.getEntity().setCustomName("Technoblade");
+                event.getEntity().setCustomNameVisible(true);
+            }
+        }
     }
 }
+
